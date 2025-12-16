98c267fcb7add17ab43ee27b0374fe448b9d3087
Kolosov Egor <e.kolosov@g.nsu.ru>
2025-12-16 21:15:10 +0700

    serv

--- kolosov/Task32/server.c
+++ kolosov/Task32/server.c
@@ -1,175 +1,208 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <sys/time.h>
-#include <time.h>
-#include <aio.h>
+#include <ctype.h>
+#include <poll.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <signal.h>
 
-#define SOCK_PATH "socket_path"
-#define MAX_CLIENTS 32
-#define MAX_OUT 200000
+#define SOCKET_PATH  "./socket"
+#define BUF 1024
+#define MAX_CLIENTS 5
+#define CHUNK_SIZE 30
 
-struct client {
-    int fd;
-    struct aiocb aio;
-    char buf[1024];
-    int id;
-    int msg_count;
-};
+static volatile sig_atomic_t g_stop = 0;
 
-int main(void)
-{
-    int srv, newfd;
-    struct sockaddr_un addr;
-    struct client clients[MAX_CLIENTS];
+static void on_sigint(int sig) {
+    (void)sig;
+    g_stop = 1;
+}
 
-    char outbuf[MAX_OUT];
-    int outlen = 0;
+static void set_nonblocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1) {
+        perror("fcntl F_GETFL");
+        exit(1);
+    }
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        perror("fcntl F_SETFL");
+        exit(1);
+    }
+}
 
-    int i, n;
+static void now_hms(char out[16]) {
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    if (!tm) {
+        strcpy(out, "??:??:??");
+        return;
+    }
+    snprintf(out, 16, "%02d:%02d:%02d", tm->tm_hour, tm->tm_min, tm->tm_sec);
+}
+
+int main(void) {
+    int server_fd, client_fd;
+    struct sockaddr_un socket_addr;
+
+    struct pollfd fds[MAX_CLIENTS + 1];
+    int client_id[MAX_CLIENTS + 1];
+    time_t connect_time[MAX_CLIENTS + 1];
+
+    int client_count = 1;
     int next_id = 1;
 
-    time_t start;
-    struct timeval start_tv;
-    struct timeval first, last;
-    int have_first = 0;
+    char mixed_buffer[BUF * 10];
+    int mixed_pos = 0;
+    int last_id = 0;
 
-    for (i = 0; i < MAX_CLIENTS; i++) {
-        clients[i].fd = -1;
-        clients[i].id = 0;
-        clients[i].msg_count = 0;
+    signal(SIGINT, on_sigint);
+    signal(SIGTERM, on_sigint);
+
+    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("socket");
+        return 1;
+    }
+    set_nonblocking(server_fd);
+
+    memset(&socket_addr, 0, sizeof(socket_addr));
+    socket_addr.sun_family = AF_UNIX;
+    strncpy(socket_addr.sun_path, SOCKET_PATH, sizeof(socket_addr.sun_path) - 1);
+
+    unlink(SOCKET_PATH);
+
+    if (bind(server_fd, (struct sockaddr*)&socket_addr, sizeof(socket_addr)) < 0) {
+        perror("bind");
+        return 1;
+    }
+    if (listen(server_fd, MAX_CLIENTS) < 0) {
+        perror("listen");
+        return 1;
     }
 
-    srv = socket(AF_UNIX, SOCK_STREAM, 0);
-
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    strcpy(addr.sun_path, SOCK_PATH);
-    unlink(SOCK_PATH);
-
-    bind(srv, (struct sockaddr *)&addr, sizeof(addr));
-    listen(srv, 5);
+    memset(fds, 0, sizeof(fds));
+    fds[0].fd = server_fd;
+    fds[0].events = POLLIN;
 
     {
-        int flags = fcntl(srv, F_GETFL, 0);
-        fcntl(srv, F_SETFL, flags | O_NONBLOCK);
+        char t[16];
+        now_hms(t);
+        printf("[%s] server: started, socket=%s\n", t, SOCKET_PATH);
+        fflush(stdout);
     }
 
-    start = time(NULL);
-    gettimeofday(&start_tv, NULL);
-
-    while (1) {
-        if (time(NULL) - start >= 8)
+    while (!g_stop) {
+        int ready = poll(fds, client_count, 100);
+        if (ready < 0) {
+            if (errno == EINTR) continue;
+            perror("poll");
             break;
-
-        newfd = accept(srv, NULL, NULL);
-        if (newfd >= 0) {
-            for (i = 0; i < MAX_CLIENTS; i++) {
-                if (clients[i].fd < 0) {
-                    clients[i].fd = newfd;
-                    clients[i].id = next_id++;
-                    clients[i].msg_count = 0;
-
-                    memset(&clients[i].aio, 0, sizeof(struct aiocb));
-                    clients[i].aio.aio_fildes = newfd;
-                    clients[i].aio.aio_buf = clients[i].buf;
-                    clients[i].aio.aio_nbytes = sizeof(clients[i].buf);
-
-                    aio_read(&clients[i].aio);
-                    break;
-                }
-            }
         }
+        if (ready == 0) continue;
 
-        for (i = 0; i < MAX_CLIENTS; i++) {
-            if (clients[i].fd >= 0) {
-                int err = aio_error(&clients[i].aio);
+        for (int i = 0; i < client_count; i++) {
+            if (fds[i].revents == 0) continue;
 
-                if (err == 0) {
-                    struct timeval now;
-                    long sec, usec;
-                    int len, j;
-                    int cid;
+            if (fds[i].fd == server_fd) {
+                if (fds[i].revents & POLLIN) {
+                    while (1) {
+                        client_fd = accept(server_fd, NULL, NULL);
+                        if (client_fd == -1) {
+                            if (errno == EAGAIN || errno == EWOULDBLOCK) break;
+                            perror("accept");
+                            break;
+                        }
 
-                    n = aio_return(&clients[i].aio);
-                    if (n <= 0) {
-                        close(clients[i].fd);
-                        clients[i].fd = -1;
-                        clients[i].id = 0;
-                        clients[i].msg_count = 0;
-                        continue;
-                    }
+                        if (client_count < MAX_CLIENTS + 1) {
+                            set_nonblocking(client_fd);
 
-                    gettimeofday(&now, NULL);
+                            fds[client_count].fd = client_fd;
+                            fds[client_count].events = POLLIN;
 
-                    if (!have_first) {
-                        first = now;
-                        have_first = 1;
-                    }
-                    last = now;
+                            client_id[client_count] = next_id++;
+                            connect_time[client_count] = time(NULL);
 
-                    sec = now.tv_sec - start_tv.tv_sec;
-                    usec = now.tv_usec - start_tv.tv_usec;
-                    if (usec < 0) {
-                        sec--;
-                        usec += 1000000;
-                    }
+                            char t[16];
+                            now_hms(t);
+                            printf("[%s] + connect: id=%d fd=%d (clients=%d)\n",
+                                   t, client_id[client_count], client_fd, client_count);
+                            fflush(stdout);
 
-                    cid = clients[i].id;
-                    clients[i].msg_count++;
-
-                    if (outlen < MAX_OUT) {
-                        len = snprintf(outbuf + outlen,
-                                       MAX_OUT - outlen,
-                                       "[%ld.%06ld] %d message from (%d client): ",
-                                       sec, usec, clients[i].msg_count, cid);
-                        if (len > 0) {
-                            outlen += len;
-                            if (outlen > MAX_OUT)
-                                outlen = MAX_OUT;
+                            client_count++;
+                        } else {
+                            close(client_fd);
                         }
                     }
+                }
+            } else if (fds[i].revents & POLLIN) {
+                char ch;
+                ssize_t bytes = read(fds[i].fd, &ch, 1);
 
-                    for (j = 0; j < n && outlen < MAX_OUT; j++) {
-                        char c = clients[i].buf[j];
-                        c = (char)toupper((unsigned char)c);
-                        outbuf[outlen++] = c;
+                if (bytes > 0) {
+                    ch = (char)toupper((unsigned char)ch);
+
+                    if (mixed_pos < (int)sizeof(mixed_buffer) - 1) {
+                        mixed_buffer[mixed_pos++] = ch;
+                        mixed_buffer[mixed_pos] = '\0';
                     }
+                    last_id = client_id[i];
 
-                    if (outlen < MAX_OUT)
-                        outbuf[outlen++] = '\n';
+                    if (mixed_pos >= CHUNK_SIZE) {
+                        char t[16];
+                        now_hms(t);
+                        printf("[%s] message: id=%d == %s\n", t, last_id, mixed_buffer);
+                        fflush(stdout);
+                        mixed_pos = 0;
+                    }
+                } else if (bytes == -1) {
+                    if (!(errno == EAGAIN || errno == EWOULDBLOCK)) {
+                        close(fds[i].fd);
+                        fds[i].fd = -1;
+                    }
+                } else if (bytes == 0) {
+                    char t[16];
+                    now_hms(t);
 
-                    memset(&clients[i].aio, 0, sizeof(struct aiocb));
-                    clients[i].aio.aio_fildes = clients[i].fd;
-                    clients[i].aio.aio_buf = clients[i].buf;
-                    clients[i].aio.aio_nbytes = sizeof(clients[i].buf);
-                    aio_read(&clients[i].aio);
+                    int secs = (int)difftime(time(NULL), connect_time[i]);
+                    printf("[%s] - disconnect: id=%d fd=%d (alive=%ds)\n",
+                           t, client_id[i], fds[i].fd, secs);
+                    fflush(stdout);
+
+                    close(fds[i].fd);
+                    fds[i].fd = -1;
                 }
             }
         }
 
-        usleep(10000);
-    }
-
-    if (outlen > 0)
-        write(1, outbuf, outlen);
-
-    if (have_first) {
-        long sec = last.tv_sec - first.tv_sec;
-        long usec = last.tv_usec - first.tv_usec;
-        if (usec < 0) {
-            sec--;
-            usec += 1000000;
+        // compact (как у тебя)
+        for (int i = 0; i < client_count; i++) {
+            if (fds[i].fd == -1) {
+                for (int j = i; j < client_count - 1; j++) {
+                    fds[j] = fds[j + 1];
+                    client_id[j] = client_id[j + 1];
+                    connect_time[j] = connect_time[j + 1];
+                }
+                i--;
+                client_count--;
+            }
         }
-        printf("%ld.%06ld s\n", sec, usec);
     }
 
-    close(srv);
-    unlink(SOCK_PATH);
+    {
+        char t[16];
+        now_hms(t);
+        printf("[%s] server: stopping...\n", t);
+        fflush(stdout);
+    }
+
+    for (int i = 0; i < client_count; i++) {
+        if (fds[i].fd != -1) close(fds[i].fd);
+    }
+    unlink(SOCKET_PATH);
     return 0;
 }
